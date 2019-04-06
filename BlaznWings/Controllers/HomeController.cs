e964878d217edefc22b486be1a1691b79bb61015
caseygruse <36206092+caseygruse@users.noreply.github.com>
2019-04-06 14:07:59 -0700

    Setting up add and delete photo functions within portal

--- BlaznWings/Controllers/HomeController.cs
+++ BlaznWings/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using BlaznWings.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace BlaznWings.Controllers
 {
@@ -54,6 +58,86 @@ namespace BlaznWings.Controllers
 		[HttpGet]
 		public ActionResult AdminLanding()
 		{
+			return View();
+		}
+
+		[HttpGet]
+		public ActionResult ManagePhotos()
+		{
+			return View();
+		}
+		
+		[HttpGet]
+		public ActionResult AddPhotos()
+		{
+
+			return View();
+		}
+
+		[HttpPost]
+		public async System.Threading.Tasks.Task<ActionResult> AddPhotosAsync(HttpPostedFileBase file)
+		{
+			if (file != null)
+			{
+				CloudStorageAccount storageAccount = new CloudStorageAccount(
+				new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials("blaznart", "9Mh4A2zUSK8QgDOCKwf/OL7uqAtFDG90uoPNpJYXAlteZVqJU6xkN9/IP1zWVoV0l2WfFce4ksZ7t4fFHLXeaA=="), true);
+
+				CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+				CloudBlobContainer container = blobClient.GetContainerReference("blaznphotos");
+
+				// Get a reference to a blob named "myblob".
+				CloudBlockBlob blockBlob = container.GetBlockBlobReference("myblob");
+				
+
+				string imageName = Guid.NewGuid().ToString() + "-" + Path.GetExtension(file.FileName);
+
+				blockBlob = container.GetBlockBlobReference(imageName);
+				blockBlob.Properties.ContentType = file.ContentType;
+
+				await blockBlob.UploadFromStreamAsync(file.InputStream);
+
+
+				ViewBag.Message = "Image uploaded!";
+
+				return RedirectToAction("AddPhotos");
+			}
+			else
+			{
+				ViewBag.Message = "Please select a file.";
+
+				return RedirectToAction("AddPhotos");
+			}
+		}
+
+		[HttpGet]
+		public async System.Threading.Tasks.Task<ActionResult> EditDeletePhotosAsync()
+		{
+			CloudStorageAccount storageAccount = new CloudStorageAccount(
+				new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials("blaznart", "9Mh4A2zUSK8QgDOCKwf/OL7uqAtFDG90uoPNpJYXAlteZVqJU6xkN9/IP1zWVoV0l2WfFce4ksZ7t4fFHLXeaA=="), true);
+
+			CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+			CloudBlobContainer container = blobClient.GetContainerReference("blaznphotos");
+
+			List<string> images = new List<string>();
+
+			BlobContinuationToken token = null;
+			do
+			{
+				BlobResultSegment resultSegment = await container.ListBlobsSegmentedAsync(token);
+				token = resultSegment.ContinuationToken;
+
+				foreach (IListBlobItem item in resultSegment.Results)
+				{
+					images.Add(item.Uri.ToString());
+				}
+			}
+			while (token != null);
+
+			ViewBag.Images = images;
+			ViewBag.Current = "ManagePicturesAsync";
+
 			return View();
 		}
 
